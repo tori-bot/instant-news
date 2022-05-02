5f7e7ec12e9e480802ac536a0e402e8be0abbe69
victoria makena <makenavictoria1@gmail.com>
2022-05-02 09:24:22 +0300

    add api key to start and config files

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    #generate configuraion parent class
+    API_BASE_URL=
+    API_KEY=os.environ.get('API_KEY')
---
+++ start.sh
@@ -0,0 +1,3 @@
+export API_KEY='e0a95f70604a41dc8e0d35cf0b1160dd'
+
+python3.9 manage.py server
