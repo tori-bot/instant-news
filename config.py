09832528ea272b11a71e856d83a39a7f4c50bf92
victoria makena <makenavictoria1@gmail.com>
2022-05-02 09:48:01 +0300

    configure the application

--- config.py
+++ config.py
@@ -2,5 +2,18 @@ import os
 
 class Config:
     #generate configuraion parent class
-    API_BASE_URL=
+    API_BASE_URL='https://newsapi.org/v2/everything?q=Kenya&sortBy=popularity&apiKey={}'
     API_KEY=os.environ.get('API_KEY')
+
+class ProdConfig(Config):
+    #production configuration child
+    pass
+
+class DevConfig(Config):
+    #development configuration child
+    DEBUG=True
+
+config_options={
+    'development':DevConfig,
+    'production':ProdConfig
+}
