e44c3ba6c311ab46be1b5931644d67c769e2785b
victoria makena <makenavictoria1@gmail.com>
2022-05-02 11:32:00 +0300

    add imports to views file

--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_article,get_sources,search_article
+from ..models import NewsArticle, NewsSource
+
+
