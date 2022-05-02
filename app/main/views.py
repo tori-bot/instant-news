24a61d78de1c3114473e732fdb5b9e6edf0be43c
victoria makena <makenavictoria1@gmail.com>
2022-05-02 11:56:14 +0300

    add index view function and  a search functionality

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,20 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_article,get_sources,search_article
+from ..requests import get_article,get_sources, get_trending,search_article
 from ..models import NewsArticle, NewsSource
 
 #views
 @main.route('/')
 def index():
     # view root page function
-    trending_news=get_article('source')
+    trending_news=get_trending('source')
+    popular_sources=get_sources('source')
+
+    title='Home of Instant News'
+
+    search_article=request.args.get('news_query')
+
+    if search_article:
+        return redirect(url_for('.search',source=search_article))
+    else:
+        return render_template('index.html',title=title,trending_news=trending_news,popular_sources=popular_sources)
