6a5dd66fda9a5d348b334b7471d0f61903eec93a
victoria makena <makenavictoria1@gmail.com>
2022-05-02 14:48:28 +0300

    register the blueprint in app factory function

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -8,5 +8,12 @@ def create_app(config_name):
     #register a blueprint
     app=Flask(__name__)
 
-    app.config.from_object
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
 
