a9343fcd9c11d86d8fc199eb977d575029d9687e
victoria makena <makenavictoria1@gmail.com>
2022-05-02 10:18:13 +0300

    get news sources results from api request in json and convert data to py dictionary

--- app/requests.py
+++ app/requests.py
@@ -5,6 +5,7 @@ api_key=None
 base_url=None
 
 def configure_request(app):
+    #function to configure the  apirequests for the application
     global api_key,base_url
     api_key=app.config['API_KEY']
     base_url=app.config['BASE_URL']
@@ -12,3 +13,9 @@ def configure_request(app):
 def get_sources(sources):
     #this function with arg:sources gets json response to our url request
     get_sources_url=base_url.format(sources,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data=url.read()
+        get_sources_response=json.loads(get_sources_data)
+
+        sources_results=None
