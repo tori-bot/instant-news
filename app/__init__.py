bd0b414f8329e60556131db9f5215a482125e17b
victoria makena <makenavictoria1@gmail.com>
2022-05-03 09:28:37 +0300

    create tests for source class

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from ensurepip import bootstrap
+
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
---
+++ tests/test_source.py
@@ -0,0 +1,10 @@
+import unittest
+from app.models import NewsSource
+
+class SourceTest(unittest.Testcase):
+    # test case for behaviour of source class
+    def setup(self):
+        #method to run before every test
+        self.new_source=NewsSource('1234','bbc','we live for news','general','www.bbc.com','en','uk')
+
+        
