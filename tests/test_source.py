b684de217d09e6cdd48ffcd266d82c2a1018f43b
victoria makena <makenavictoria1@gmail.com>
2022-05-03 09:52:46 +0300

    finish source tests

--- tests/test_source.py
+++ tests/test_source.py
@@ -7,4 +7,15 @@ class SourceTest(unittest.Testcase):
         #method to run before every test
         self.new_source=NewsSource('1234','bbc','we live for news','general','www.bbc.com','en','uk')
 
-    def test_instance(self):      
+    def test_init(self):
+        #chech instanciation of object
+        self.assertEqual(self.new_source.index,1234)   
+        self.assertEqual(self.new_source.org_name,'bbc')
+        self.assertEqual(self.new_source.description,'we live for news') 
+        self.assertEqual(self.new_source.category,'general')
+        self.assertEqual(self.new_source.url,'www.bbc.com')
+        self.assertEqual(self.new_source.language,'en')
+        self.assertEqual(self.new_source.country,'uk')  
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,NewsSource))
